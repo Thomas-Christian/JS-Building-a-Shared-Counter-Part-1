a749cdc73cd242daf61113a9107f95e52831c357
Tom C <thomaspchristian@outlook.com>
2022-07-07 19:53:47 -0400

    meop

--- public/index.js
+++ public/index.js
@@ -3,7 +3,23 @@ function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
+    async function getData() {
+
+        let response = await fetch('http://localhost:9001/counter');
+
+        let data = await response.json();
+
+            console.log(data); 
+        
+        countValue = await data.value;
+
+        countContainer.textContent = countValue;
+            
+    }
+
     let countValue = 0;
+
+    getData();
 
     function increment(){
         countValue++;
