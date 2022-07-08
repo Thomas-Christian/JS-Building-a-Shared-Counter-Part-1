ff69b1dbb4ec4952f59c6567c08a86821e2ff4bc
Tom C <thomaspchristian@outlook.com>
2022-07-07 20:49:14 -0400

    counter to server

--- public/index.js
+++ public/index.js
@@ -1,38 +1,46 @@
-function main(){
-    const countContainer = document.querySelector('#count-container');
-    const incrementButton = document.querySelector('#increment-button');
-    const decrementButton = document.querySelector('#decrement-button');
+function main() {
+  const countContainer = document.querySelector("#count-container");
+  const incrementButton = document.querySelector("#increment-button");
+  const decrementButton = document.querySelector("#decrement-button");
 
-    async function getData() {
-
-        let response = await fetch('http://localhost:9001/counter');
-
-        let data = await response.json();
-
-            console.log(data); 
-        
-        countValue = await data.value;
-
-        countContainer.textContent = countValue;
-            
-    }
-
-    let countValue = 0;
-
-    getData();
-
-    function increment(){
-        countValue++;
-        countContainer.textContent = countValue;
-    }
-
-    function decrement(){
-        countValue--;
-        countContainer.textContent = countValue;
-    }
-
-    incrementButton.addEventListener('click', increment);
-    decrementButton.addEventListener('click', decrement);
+  async function getData() {
+    let response = await fetch("http://localhost:9001/counter");
+    let data = await response.json();
+    console.log(data);
+    countValue = await data.value;
     countContainer.textContent = countValue;
+  }
+
+  let countValue = 0;
+
+  getData();
+
+  async function increment() {
+    countValue++;
+    countContainer.textContent = countValue;
+    let response = await fetch("http://localhost:9001/counter", {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            value: countValue
+        })
+    })
+  }
+
+  async function decrement() {
+    countValue--;
+    countContainer.textContent = countValue;
+    let response = await fetch("http://localhost:9001/counter", {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            value: countValue
+        })
+    })
+  }
+
+  incrementButton.addEventListener("click", increment);
+  decrementButton.addEventListener("click", decrement);
+  countContainer.textContent = countValue;
 }
-main()
+main();
